e5a7a325f7c956f29bf806e8d1992799f16f6be8
Rh4096 <rhelvetican@gmail.com>
2024-01-17 14:40:18 +0700

    second

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ const T_F: &str = "True/False";
 #[clap(author, version, about, long_about = None)]
 struct Args {
     #[clap(short, long, default_value_t = 8, value_name = NUMBER, display_order = 1)]
-    number: u8,
+    length: u8,
     #[clap(short, long, value_parser, default_value_t = true, value_name = T_F, display_order = 2)]
     number_enabled: bool,
     #[clap(short, long, value_parser, default_value_t = false, value_name = T_F, display_order = 3)]
@@ -17,4 +17,32 @@ struct Args {
 }
 
 fn main() {
+    let args = Args::parse();
+
+    let length: u8 = args.length;
+    let number_enabled: bool = args.number_enabled;
+    let capital_enabled: bool = args.capital_enabled;
+    let special_enabled: bool = args.special_enabled;
+
+    let mut char_pool: Vec<char> = Vec::new();
+
+    char_pool.extend('a'..='z');
+    if capital_enabled {
+        char_pool.extend('A'..='Z');
+    }
+    if number_enabled {
+        char_pool.extend('0'..='9');
+    }
+    if special_enabled {
+        char_pool.extend("!@#$%^&*()".chars());
+    }
+
+    let mut password: String = String::new();
+
+    for _ in 0..length {
+        let random_index: usize = rand::random::<usize>() % char_pool.len();
+        password.push(char_pool[random_index]);
+    }
+
+    println!("{}", password);
 }
